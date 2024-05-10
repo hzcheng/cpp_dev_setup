3d27f12a4516f626b357fa72f25c73d4a1117bca
Hongze Cheng <hzcheng@taosdata.com>
2024-05-10 18:08:18 +0800

    more

--- setup.sh
+++ setup.sh
@@ -47,6 +47,7 @@ for ((i=1; i<=NUM_CONTAINERS; i++)); do
     echo '				"github.vscode-pull-request-github",' >> ${CONTAINER_CONFIG_FILE}
     echo '				"llvm-vs-code-extensions.vscode-clangd"' >> ${CONTAINER_CONFIG_FILE}
     echo '				"liangqin.quick-notes"' >> ${CONTAINER_CONFIG_FILE}
+    echo '				"yzhang.markdown-all-in-one"' >> ${CONTAINER_CONFIG_FILE}
     echo '			]' >> ${CONTAINER_CONFIG_FILE}
     echo '		}' >> ${CONTAINER_CONFIG_FILE}
     echo '	},' >> ${CONTAINER_CONFIG_FILE}
